912f78d7d0d1417ff564727b72c2907a67010378
Kevin Henriquez <kevin@MacBook-Pro-de-Kevin.local>
2017-04-19 20:04:15 -0400

    Changes
    Layout css, updated functionality (validation)

--- app/controller.js
+++ app/controller.js
@@ -12,30 +12,38 @@ angular.module('paypalConverter.convert', [])
 }])
 
 .controller('MainController', ['convertService', '$scope', function(convertService, $scope) {
-  $scope.message = "Not chosen anything yet!";
-  var total = 0;
+  $scope.message = "Enter an amount and select a currency";
+  $scope.options = [
+    {id: 0, name: 'CAD'},
+    {id: 1, name: 'USD'}
+  ];
   var rate = 0;
+  var paypal_comission = 0.9456;
+  var paypal_conv_comission = 0.9624;
+  var multicaja_comission = 10;
+  var multicaja_conv_comission = 0.9755;
     convertService.getConversionRates().then(function(response) {
       $scope.data = response.data;
     }, function(response){
       console.log('Some error ocurred');
+      alert("Some error ocurred");
     });
 
     $scope.showConverted = function(value, currency) {
-      switch (currency) {
-        case "CAD":
+      var usd_clp = $scope.data.rates.CLP;
+      switch (currency.name) {
+        case 'CAD':
           rate = 1 / ($scope.data.rates.CAD);
           break;
-        case "USD":
+        case 'USD':
           rate = 1;
           break;
         default:
           rate = 1;
       }
-      total = value * rate;
-      $scope.message = "Your total is " + total;
+      $scope.total = ((value * paypal_comission * rate * paypal_conv_comission) - multicaja_comission) * usd_clp * multicaja_conv_comission;
+      $scope.message = "Your converted amount is ";
+      $scop
     }
-
-  $scope.hello = "Hello World!";
 
 }]);
