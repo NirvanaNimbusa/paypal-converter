27665dd9274e1e62eb50aafc3f9f7375879b1102
Kevin Henriquez <kevinkonrad93@gmail.com>
2017-04-19 20:40:42 -0400

    Created dist folder

--- gulpfile.js
+++ gulpfile.js
@@ -10,6 +10,12 @@ var neat = require('node-neat');
 var browserSync = require('browser-sync');
 var autoprefixer = require('gulp-autoprefixer');
 var reload = browserSync.reload;
+var useref = require('gulp-useref');
+var uglify = require('gulp-uglify');
+var gulpIf = require('gulp-if');
+var cssnano = require('gulp-cssnano');
+var del = require('del');
+var runSequence = require('run-sequence');
 
 gulp.task('js', function() {
 	return gulp.src(['./app/js/*.js'], ['!./app/js/concat.js'])
@@ -43,6 +49,26 @@ gulp.task('browser-sync', function() {
 			baseDir: "./app"
 		}
 	});
+});
+
+gulp.task('useref', function(){
+  return gulp.src('app/*.html')
+    .pipe(useref())
+    .pipe(gulpIf('*.js', uglify()))
+    // Minifies only if it's a CSS file
+    .pipe(gulpIf('*.css', cssnano()))
+    .pipe(gulp.dest('dist'))
+});
+
+gulp.task('clean:dist', function() {
+  return del.sync('dist');
+});
+
+gulp.task('build', function (callback) {
+  runSequence('clean:dist',
+    ['sass', 'useref'],
+    callback
+  )
 });
 
 gulp.task('default', ['js', 'sass', 'browser-sync'], function () {
